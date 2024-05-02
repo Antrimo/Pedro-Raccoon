7aeb34ed228d8fb2c4bfc2f7ba66ace893533f53
Kartikey Srivastava <kartikeyai246@gmail.com>
2024-05-02 12:53:47 +0530

    inital commit

---
+++ android/app/src/main/kotlin/com/example/raccoon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.raccoon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/raccoon.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Raccoon extends StatefulWidget {
+  const Raccoon({super.key});
+
+  @override
+  State<Raccoon> createState() => _RaccoonState();
+}
+
+class _RaccoonState extends State<Raccoon> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black26,
+      body: Center(
+        child: Container(
+          height: 300,
+          width: 300,
+          // alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(150),
+          ),
+        ),
+      ),
+    );
+  }
+}
